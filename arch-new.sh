5610e350a78845d6b28cd4d170af397b4d1010e2
Derek Chafin <infomaniac50@gmail.com>
2013-11-13 10:51:28 -0600

    Silenced curl and added some whitespace.

--- arch-new.sh
+++ arch-new.sh
@@ -4,9 +4,10 @@ source arch-common.sh
 
 arch_term
 
-curl http://pastebin.ca/raw/2404700 | tr -d "\r" > pacmrr
+curl -Ss http://pastebin.ca/raw/2404700 | tr -d "\r" > pacmrr
 bash pacmrr
 
+echo ""
 echo "We are done for now."
 echo "You should make your disk partitions now."
 echo "Run arch-fresh.sh after you have mounted your new filesystems."
