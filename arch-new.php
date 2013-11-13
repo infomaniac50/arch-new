44f5de2e24db84387fb651a13a73033e486f8fc4
Derek Chafin <infomaniac50@gmail.com>
2013-11-13 16:02:47 -0600

    Renamed first script file to start.sh to keep it consistent with my other bootstrap scripts.

--- arch-new.php
+++ arch-new.php
@@ -1,8 +1,11 @@
 <?php
 
-echo 'curl -Ss http://coding-squared.com/arch-new.tar.gz | tar -xzf -';
-echo "\n";
-echo 'chown root:root *.sh';
-echo "\n";
-echo 'echo "Please run arch-new.sh"';
-echo "\n";
+header("Content-Type: text/plain");
+
+function println($string) {
+	echo $string;
+	echo "\n";
+}
+
+println('curl -Ss http://coding-squared.com/arch-new.tar.gz | tar -xz --no-same-owner --no-same-permissions -f -');
+println('echo "Please run \'bash start.sh\'"');
